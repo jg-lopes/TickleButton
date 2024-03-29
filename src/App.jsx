a7e207cc034e33fa4c85aec11a30c87b9721369c
João Gabriel Lopes <30786108+jg-lopes@users.noreply.github.com>
2024-03-29 00:49:02 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function Button() {
+  const [tickleFactor, setTickleFactor] = useState(0);
+
+  const buttonStyle = {
+    "margin-left": getRandomInt(-tickleFactor, tickleFactor).toString() + "px",
+    "margin-top": getRandomInt(-tickleFactor, tickleFactor).toString() + "px",
+  };
+
+  return (
+    <div class="display-room">
+      <div class="tickle-room">
+        <button
+          style={buttonStyle}
+          onClick={() => setTickleFactor(tickleFactor + 50)}
+          id="tickle"
+        >
+          Tickle!
+        </button>
+      </div>
+      <div class="ui">
+        <p>
+          <button onClick={() => setTickleFactor(0)}>
+            Reset Tickle Factor!
+          </button> - Tickle Factor: {tickleFactor}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <main>
+      <Button />
+    </main>
+  );
+}
